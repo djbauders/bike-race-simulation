f87a52f769f88f6b345ebe14100ae8c885907981
gitgud115 <jaredscottdunham@gmail.com>
2021-12-01 02:44:59 -0500

    Added JUnit tests for calcVelocity and calcRollingFriciton

---
+++ src/Default/SimulationSpec.java
@@ -0,0 +1,27 @@
+package Default;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class SimulationSpec {
+	
+	@Test
+	void velocityWithCylcistAlecAtBaseMeasurementsIs10_1744() throws Exception{
+		Weather weatherObj = new Weather("Rain", 0, 23.88, "Foggy");
+		RaceCourse course = new RaceCourse(1, 1, 0, 0, "Test", "Test", weatherObj);
+		Cyclist Alec = new Cyclist("Alec", 59.87, 150, 0.4387, 303, "Climber", new Bike("Trek", "Madone SLR 9", 2021, 7.5, 0.00330, "Carbon", "Road Race"));
+		
+		assertTrue(Simulation.calculateVeloctiy(Alec, course, weatherObj) == 10.174405920296387);
+	}
+	
+	@Test
+	void rollingFrictionWithCylistAlecIS3_1596048() throws Exception {
+		Weather weatherObj = new Weather("Rain", 0, 23.88, "Foggy");
+		RaceCourse course = new RaceCourse(1, 1, 0, 0, "Test", "Test", weatherObj);
+		Cyclist Alec = new Cyclist("Alec", 59.87, 150, 0.4387, 303, "Climber", new Bike("Trek", "Madone SLR 9", 2021, 7.5, 0.00330, "Carbon", "Road Race"));
+		
+		assertTrue(Simulation.calcRollingFriction(Alec, course) == 2.18096901);
+	}
+
+}
